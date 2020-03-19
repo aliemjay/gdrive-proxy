163c6a4913734a55d10d90c7cbe8c465eb1e7b6c
Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>
2020-03-19 13:30:33 +0300

    mass update to publish

--- gdrive-proxy.py
+++ gdrive-proxy.py
@@ -1,33 +1,54 @@
+import argparse
+import logging
+import os.path
+import re
+import socket
+import socketserver
+import urllib.request
 from functools import partial
 from html.parser import HTMLParser
 from http.client import HTTPResponse, IncompleteRead
+from http.cookiejar import MozillaCookieJar
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from urllib.request import urlopen, Request
-from http.cookiejar import CookieJar, DefaultCookiePolicy
+from threading import Lock
 from urllib.error import URLError
-import urllib.request
-import socketserver
-import logging
-import re
+from urllib.request import Request
 
 log = logging.getLogger()
 
 
 class ConfirmPageParser(HTMLParser):
     def __init__(self):
-        super().__init__()
+        super().__init__(convert_charrefs=True)
+        self.is_error = True
+        self.error_msg = ""
         self.download_path = None
+        self._expect_error_msg = False
 
-    def handle_tag(self, tag, attrs):
+    def handle_tag(self, tag, attrs, is_startend=False):
         attrs = dict(attrs)
         if attrs.get('id') == 'uc-download-link':
+            self.is_error = False
             self.download_path = attrs.get('href')
+        elif not is_startend and attrs.get('id') in \
+                ['uc-error-caption', 'uc-error-subcaption']:
+            self.is_error = True
+            self._expect_error_msg = True
 
     def handle_starttag(self, tag, attrs):
         self.handle_tag(tag, attrs)
 
     def handle_startendtag(self, tag, attrs):
-        self.handle_tag(tag, attrs)
+        self.handle_tag(tag, attrs, is_startend=True)
+
+    def handle_data(self, data):
+        if self._expect_error_msg:
+            self._expect_error_msg = False
+            self.error_msg += data + " "
+
+
+class GDriveError(Exception):
+    pass
 
 
 class GDriveSession:
@@ -40,11 +61,36 @@ class GDriveSession:
             urllib.request.HTTPCookieProcessor(cookiejar=cookiejar))
         self.timeout = timeout
         self.user_agent = user_agent
+        self.lock = Lock()
+        self.cache = {}
 
     def urlopen(self, request: Request) -> HTTPResponse:
         return self.opener.open(request, timeout=self.timeout)
 
-    def open_file(self, id_, headers=None) -> HTTPResponse:
+    def _open_file(self, id_, headers=None) -> HTTPResponse:
+        download_url = self.root + (self.download_path % id_)
+        request = Request(download_url, headers=headers)
+        response = self.urlopen(request)
+
+        if 'Content-Disposition' not in response.msg and \
+                'text/html' in response.getheader('Content-Type', ''):
+            # confirm page?? parse it!
+            parser = ConfirmPageParser()
+            with response:
+                parser.feed(response.read().decode('utf-8'))
+            parser.close()
+            if parser.is_error:
+                raise GDriveError("couldn't bypass webpage %s. "
+                                  "got this message: %s" %
+                                  (download_url, parser.error_msg))
+
+            new_download_url = self.root + parser.download_path
+            log.debug("following url from confirm page: %s" % new_download_url)
+            response = self.urlopen(Request(new_download_url, headers=headers))
+
+        return response
+
+    def open_file(self, id_, headers=None):
         if headers is None:
             headers = {}
         if self.user_agent:
@@ -53,21 +99,22 @@ class GDriveSession:
                            if key.lower() != 'user-agent')
             headers['User-Agent'] = self.user_agent
 
-        download_url = self.root + (self.download_path % id_)
-        request = Request(download_url, headers=headers)
-        response = self.urlopen(request)
+        # serialize to avoid spamming and protect cache
+        with self.lock:
+            if id_ in self.cache:
+                # use cache
+                log.debug("trying cached url %s for %s", self.cache[id_], id_)
+                request = Request(self.cache[id_], headers=headers)
+                try:
+                    response = self.urlopen(request)
+                except URLError:
+                    pass
+                else:
+                    log.info("cached url success %s", id_)
+                    return response
 
-        if response.status in [200, 206] and \
-                'Content-Disposition' not in response.msg and \
-                'text/html' in response.getheader('Content-Type', ''):
-            # confirm page?? parse it!
-            parser = ConfirmPageParser()
-            parser.feed(response.read().decode('utf-8'))
-            response.close()
-            new_download_url = self.root + parser.download_path
-            return self.urlopen(Request(new_download_url, headers=headers))
-        else:
-            # pass as is
+            response = self._open_file(id_, headers)
+            self.cache[id_] = response.geturl()
             return response
 
 
@@ -84,8 +131,7 @@ def response_length(resp: HTTPResponse):
     if resp.getcode() == 206 and 'content-range' in headers:
         match = re.match(r' *bytes *(\d+) *- *(\d+)', headers['content-range'])
         if match is None:
-            raise RuntimeError("unexpected content-range: %s" %
-                               headers['content-range'])
+            raise RuntimeError("unexpected content-range: %s" % headers['content-range'])
         start_byte, end_byte = match.groups()
         return int(end_byte) - int(start_byte) + 1
     else:
@@ -117,13 +163,13 @@ class GDriveProxyRequestHandler(BaseHTTPRequestHandler):
         super().__init__(request, client_address, server)
 
     def log_message(self, format, *args):
-        self.log.info("%s:%d -- %s", *self.client_address, format%args)
+        self.log.info("%s:%d -- %s", *self.client_address, format % args)
 
     def log_error(self, format, *args):
-        self.log.error("%s:%d -- %s", *self.client_address, format%args)
+        self.log.error("%s:%d -- %s", *self.client_address, format % args)
 
     def log_exception(self, format, *args):
-        self.log.exception("%s:%d -- %s", *self.client_address, format%args)
+        self.log.exception("%s:%d -- %s", *self.client_address, format % args)
 
     def relay(self, upstream: HTTPResponse):
         # add upstream response headers after sanitization
@@ -191,12 +237,13 @@ class GDriveProxyRequestHandler(BaseHTTPRequestHandler):
         try:
             upstream_response = self.gdrive.open_file(args[0],
                                                       headers=upstream_headers)
-        except (TimeoutError, ConnectionError, URLError) as e:
-            self.log_error("error communicating with gdrive: %s" % e)
-            self.send_error(504, 'Upstream GDrive Not Available')
+        except (socket.timeout, ConnectionError, URLError, GDriveError) as e:
+            self.log_error("error communicating with gdrive: %s: %s",
+                           type(e).__name__, e)
+            self.send_error(502, "Bad Gateway")
             return
         except:
-            self.log_exception("while opening gdrive resource")
+            self.log_exception("non-specific error")
             self.send_error(500, "Internal Server Error")
             return
 
@@ -207,29 +254,44 @@ class GDriveProxyRequestHandler(BaseHTTPRequestHandler):
 class ThreadingHTTPServer(socketserver.ThreadingMixIn, HTTPServer):
     daemon_threads = True
 
-def main(argv):
-    import argparse
+
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', '-p', type=int, default=8989, help="server port")
-    parser.add_argument('--address', '-a', default='localhost', help="server address")
-    parser.add_argument('--timeout', '-t', type=int, default=5, help="socket timeout")
-    parser.add_argument('--user-agent', help="override user-agent string")
-    parser.add_argument('--debug', '-d', action='store_true', help="print http headers")
+    parser.add_argument('--port', '-p', type=int, default=8989,
+                        help="server port (default: %(default)s)")
+    parser.add_argument('--address', '-a', default='localhost',
+                        help="server address (default: %(default)s)")
+    parser.add_argument('--timeout', '-t', type=int, default=5,
+                        help="socket timeout (sec) (default: %(default)s)")
+    parser.add_argument('--user-agent',
+                        help="override user-agent string")
+    parser.add_argument('--debug', '-d', type=int, choices=range(3), default=0,
+                        help="debug level (2: print headers)")
+    parser.add_argument('--cookies', '-c', default='/tmp/gdrive.cookies.txt',
+                        help="cookies file (default: %(default)s)")
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG if args.debug > 0 else logging.INFO,
+                        datefmt='%H:%M:%S',
+                        format='%(asctime)s %(levelname)6s: %(name)s: %(message)s')
 
-    gdrive = GDriveSession(timeout=args.timeout, debug=args.debug, user_agent=args.user_agent)
+    cookiejar = MozillaCookieJar(args.cookies)
+    if os.path.exists(cookiejar.filename):
+        cookiejar.load()
+    gdrive = GDriveSession(timeout=args.timeout, debug=(args.debug == 2),
+                           user_agent=args.user_agent, cookiejar=cookiejar)
     request_handler = partial(GDriveProxyRequestHandler, gdrive)
     address = (args.address, args.port)
-    with ThreadingHTTPServer(address, request_handler) as server:
+    server = ThreadingHTTPServer(address, request_handler)
+    try:
         log.info("bound to address %s! serving requests...", address)
         server.serve_forever()
+    except KeyboardInterrupt:
+        log.info("interrupted! exiting...")
+    finally:
+        server.server_close()
+        cookiejar.save()
+
 
 if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
-
-# TODO use logging module; override .log(), .log_error()
-# TODO implement GDriveSession.timeout
-#
+    exit(main())
